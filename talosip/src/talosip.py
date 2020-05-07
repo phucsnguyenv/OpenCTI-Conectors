bcbd07267548c59631e2a14800bdcfb1cdb1ddcb
phucsnguyenv <50351139+phucsnguyenv@users.noreply.github.com>
2020-05-07 20:58:30 +0700

    v1.0.0 b4
    - Complete the pull code from Talos
    - Make it run by interval

--- talosip/src/talosip.py
+++ talosip/src/talosip.py
@@ -10,7 +10,8 @@ from stix2 import Indicator, IPv4Address, Bundle, ExternalReference, Report, TLP
 
 class Talosip:
     def __init__(self):
-        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
+        config_file_path = os.path.dirname(
+            os.path.abspath(__file__)) + "/config.yml"
         config = (
             yaml.load(open(config_file_path), Loader=yaml.FullLoader)
             if os.path.isfile(config_file_path)
@@ -29,7 +30,8 @@ class Talosip:
             name="Cisco Talos",
             description="Talosintilligence  IP Blacklist"
         )
-        
+        self.tags = [{"tag_type": "Event",
+                      "value": "TalosIntelligence", "color": "#fc036b"}]
 
     def get_interval(self):
         return int(self.talosip_interval) * 60 * 60 * 24
@@ -39,32 +41,36 @@ class Talosip:
         stix_indicators = []
 
         while True:
-            black_list_file = os.path.dirname(os.path.abspath(__file__))+"/ip_blacklist.txt"
+            black_list_file = os.path.dirname(
+                os.path.abspath(__file__))+"/ip_blacklist.txt"
             if os.path.isfile(black_list_file):
-                print("[31] File IP blacklist existing, deleting file...")
+                self.helper.log_info(
+                    "[48] File IP blacklist existing, deleting file...")
                 # deleting file....
                 os.remove(black_list_file)
-                print("[] File deleted.")
+                self.helper.log_info("[50] File deleted.")
             elif not os.path.isfile(black_list_file):
-                print("[33] File not exist or deleted. Downloading new file...")
+                self.helper.log_info(
+                    "[54] File not exist or deleted. Downloading new file...")
                 ip_blacklist = wget.download(
                     self.talosip_url, out="ip_blacklist.txt")
                 # processing message...
                 ip_lists = open("ip_blacklist.txt", "r")
-                print("File downloaded. Processing data...")
+                self.helper.log_info(
+                    "[59] File downloaded. Processing data...")
                 for ip in ip_lists:
                     ip = ip.strip("\n")
                     _indicator = self._create_indicator(ip)
                     stix_indicators.append(_indicator["id"])
                     stix_bundle.append(_indicator)
-         # create a report
+                # create a report
                 _report_uuid = "report--1620352a-00ff-4ab8-97cb-eeaed0779a90"
                 _report_external_reference = ExternalReference(
                     source_name="Talos Intelligence",
                     url="https://talosintelligence.com/",
                     external_id="ip-blacklist"
                 )
-
+                self.helper.log_info("Creating report...")
                 _report = Report(
                     id=_report_uuid,
                     name="Talos Intelligence IP Blacklist",
@@ -77,18 +83,17 @@ class Talosip:
                     object_refs=stix_indicators,
                 )
                 stix_bundle.append(_report)
-
+                self.helper.log_info("Sending bundle....")
                 sending_bundle = Bundle(objects=stix_bundle)
                 self.helper.send_stix2_bundle(
-                    bundle=sending_bundle.serialize(),update=True
+                    bundle=sending_bundle.serialize(), update=True
                 )
+                self.helper.log_info("Done.")
                 break
             else:
                 raise ValueError(
                     "[] Error unknown."
                 )
-
-       
 
     def _create_indicator(self, data):
         # create stix indicator
@@ -104,9 +109,58 @@ class Talosip:
         return _indicator
 
     def start(self):
-        self._process_file()
+        self.helper.log_info("[111] Fetching Talos IP database...")
+        while True:
+            try:
+                timestamp = int(time.time())
+                current_state = self.helper.get_state()
+                if current_state is not None and "last_run" in current_state:
+                    last_run = current_state["last_run"]
+                    self.helper.log_info(
+                        "[119] Connector last run: "
+                        + datetime.utcfromtimestamp(last_run).strftime(
+                            "%Y-%m-%d %H:%M:%S"
+                        )
+                    )
+                else:
+                    last_run = None
+                    self.helper.log_info("[126] Connector has never run")
+                if last_run is None or (
+                    (timestamp - last_run) > ((int(self.talosip_interval) - 1)
+                                              * 60 * 60 * 24)
+                ):
+                    self.helper.log_info("[131] Connector will run!")
+                    self._process_file()
+                    self.helper.log_info(
+                        "[134] Connector successfully run, storing last_run as "+str(timestamp))
+                    self.helper.set_state({"last_run": timestamp})
+                    self.helper.log_info(
+                        "[137] Last_run stored, next run in: "
+                        + str(round(self.get_interval() / 60 / 60 / 24, 2))
+                        + " days"
+                    )
+                    time.sleep(60)
+                else:
+                    new_interval = self.get_interval() - (timestamp - last_run)
+                    self.helper.log_info(
+                        "[145] Connector will not run, next run in: "
+                        + str(round(new_interval / 60 / 60 / 24, 2))
+                        + " days"
+                    )
+                    time.sleep(60)
+            except (KeyboardInterrupt, SystemExit):
+                self.helper.log_info("[151] Connector stop")
+                exit(0)
+            except Exception as e:
+                self.helper.log_error(str(e))
+                time.sleep(60)
 
 
 if __name__ == "__main__":
-    talosipInstance = Talosip()
-    talosipInstance.start()
+    try:
+        talosipInstance = Talosip()
+        talosipInstance.start()
+    except Exception as e:
+        print(e)
+        time.sleep(10)
+        exit(0)
