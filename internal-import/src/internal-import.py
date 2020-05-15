8c1bdb6e9afc36f804fc1e35f99e35ec197ac800
phucsnguyenv <50351139+phucsnguyenv@users.noreply.github.com>
2020-05-15 16:09:34 +0700

    v1.3.0

--- internal-import/src/internal-import.py
+++ internal-import/src/internal-import.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 from pycti import OpenCTIConnectorHelper, get_config_variable
 from pycti.utils.constants import CustomProperties
+from stix2 import Indicator, TLP_WHITE, Report, Bundle, Identity
 
 
 class InternalImport:
@@ -31,6 +32,13 @@ class InternalImport:
             type="Organization",
             description="Importing internal data from CSV file",
         )
+        self.stix_identity = Identity(
+            name="Internal Collector",
+            type="identity",
+            description="Importing internal data from CSV file",
+            identity_class="organization",
+        )
+        self.stix_tag = [{"tag_type": "Internal-Import", "value": "internal-importer", "color": "#2e99db"}]
         self.markingDefinitions = self.helper.api.marking_definition.create(
             definition_type="tlp", definition="TLP:WHITE"
         )
@@ -70,13 +78,31 @@ class InternalImport:
             "domain": "Domain",
         }
         _type = _dict.get(data.lower())
-        return _type
+        if(_type is not None):
+            return _type
+        else:
+            raise ValueError("[] Type must be url, ip, domain, md5, sha1 or sha256.")
+
+    def stix_indicator_create(self, data):
+        _type = self._get_type(data[1]).lower()
+        _value = data[0]
+        _indicator = Indicator(
+            name=_value,
+            pattern="["+_type+":value = '"+_value+"']",
+            labels="malicious-activity",
+            description="Indicator imported from {}".format(self.filename),
+            object_marking_refs=TLP_WHITE,
+            custom_properties={CustomProperties.TAG_TYPE: self.stix_tag},
+            created_by_ref=self.stix_identity
+        )
+        return _indicator
 
     def _process_message(self, data):
         """doing things with data here"""
-        created_observable_id_list = []
-        created_indicator_id_list = []
-        self.helper.log_info("Creating Indicators data")
+        created_stix_indicator_id_list = []
+        stix_bundle = []
+        stix_bundle.append(self.stix_identity)
+        self.helper.log_info("Creating Observable data")
         for row in data:
             if row[0] == "_report":
                 _report = row
@@ -89,7 +115,6 @@ class InternalImport:
                     observable_value=row[0],
                     createdByRef=self.identity["id"],
                     markingDefinitions=self.markingDefinitions["id"],
-                    createIndicator=True,
                 )
                 # create external references
                 virus_ref = self.helper.api.external_reference.create(
@@ -112,41 +137,58 @@ class InternalImport:
                     id=created_observable["id"], tag_id=self.tag["id"]
                 )
                 # this should be stix_id_key
-                created_observable_id_list.append(
-                    created_observable["stix_id_key"])
-                created_indicator_id_list.append(
-                    created_observable["indicatorsIds"])
+                created_stix_indicator = self.stix_indicator_create(row)
+                stix_bundle.append(created_stix_indicator)
+                created_stix_indicator_id_list.append(created_stix_indicator["id"])
         # Creating report
         self.helper.log_info("Generating report...")
-        created_report = self.helper.api.report.create(
-            report_class="Internal Report",
+        stix_report = Report(
+            name="Data imported from {}".format(self.filename),
+            type="report",
             description=_report[1],
-            name="Import data from file {}".format(self.filename),
             published=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            createdByRef=self.identity["id"]
+            created_by_ref=self.stix_identity,
+            object_marking_refs=TLP_WHITE,
+            labels=["threat-report"],
+            object_refs=created_stix_indicator_id_list,
+            custom_properties={CustomProperties.TAG_TYPE: self.stix_tag},
         )
-        self.helper.api.stix_entity.add_tag(
-            id=created_report["id"], tag_id=self.tag["id"]
+        stix_bundle.append(stix_report)
+        self.helper.log_info("Sending bundle....")
+        sending_stix_bundle = Bundle(objects=stix_bundle)
+        self.helper.send_stix2_bundle(
+            bundle=sending_stix_bundle.serialize(), update=True
         )
+        self.helper.log_info("STIX Bundle has been sent.")
+        # created_report = self.helper.api.report.create(
+        #     report_class="Internal Report",
+        #     description=_report[1],
+        #     name="Import data from file {}".format(self.filename),
+        #     published=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        #     createdByRef=self.identity["id"]
+        # )
+        # self.helper.api.stix_entity.add_tag(
+        #     id=created_report["id"], tag_id=self.tag["id"]
         # Adding observale to report
         # self.helper.log_info(created_observable_id_list)
-        for created_observable_stix_id in created_observable_id_list:
-            self.helper.log_info("Attaching {} to report -- {}".format(created_observable_stix_id, type(created_observable_stix_id)))
-            self.helper.api.report.add_stix_observable(
-                id=created_report["id"], stix_observable_id=created_observable_stix_id,report=created_report
-            )
+        # for created_observable_stix_id in created_observable_id_list:
+        #     self.helper.log_info("Attaching {} to report -- {}".format(created_observable_stix_id, type(created_observable_stix_id)))
+        #     self.helper.api.report.add_stix_observable(
+        #         id=created_report["id"], stix_observable_id=created_observable_stix_id,report=created_report
+        #     )
 
-        self.helper.log_info("Archiving file...")
+        self.helper.log_info("Archiving files...")
         # archiving files
         _src = self._data_path + "/files/" + self.filename
         _dest = self._data_path + "/archive"
         shutil.move(_src, _dest)
+        self.helper.log_info("Files achived...")
         self.helper.log_info("Sleeping...")
 
     def start(self):
         while True:
             self._open_files()
-            time.sleep(50)
+            time.sleep(120)
 
 
 if __name__ == "__main__":
