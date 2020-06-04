32883681c61c8db3ea451f969b631f4fda906887
phucsnguyenv <50351139+phucsnguyenv@users.noreply.github.com>
2020-06-04 09:40:51 +0700

    Update talosip.py

--- talosip/src/talosip.py
+++ talosip/src/talosip.py
@@ -165,11 +165,13 @@ class Talosip:
                     markingDefinitions=self.tlp_white_marking_definition["id"],
                     description="This report represents the blacklist provided by Cisco Talos",
                     report_class="Threat Report",
-                    createdByRef=self.identity["id"],
-                    external_reference_id=_report_external_reference["id"],
+                    createdByRef=self.identity["id"]
                 )
-                self.helper.log_info("Creating External reference...")
-
+                self.helper.log_info("Adding External reference...")
+                self.helper.api.add_external_reference(
+                    id=created_report["id"], external_reference_id=_report_external_reference["id"]
+                )
+                
                 self.helper.log_info("Adding observables to report...")
                 for observable_id in created_observable_id:
                     self.helper.api.report.add_stix_observable(
