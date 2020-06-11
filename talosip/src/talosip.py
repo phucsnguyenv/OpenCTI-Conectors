0712a097c3318fa5f3a30f89d109d605741908de
phucsnguyenv <50351139+phucsnguyenv@users.noreply.github.com>
2020-06-11 15:29:49 +0700

    Update talosip.py

--- talosip/src/talosip.py
+++ talosip/src/talosip.py
@@ -94,6 +94,8 @@ class Talosip:
         self.helper.log_info(new_iplist)
         old_iplist = old_iplist.read()
         new_iplist = new_iplist.read()
+        self.helper.log_info(old_iplist)
+        self.helper.log_info(new_iplist)
         parsed_old_list = []
         parsed_new_list = []
         for ip in old_iplist:
